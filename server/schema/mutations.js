b66fa1f914d8e6b204eecc723dfe38c774e9e562
David <djmiller1008@yahoo.com>
2023-01-31 23:06:20 +0800

    Add basic create and delete mutations for categories and products

---
+++ server/schema/mutations.js
@@ -0,0 +1,58 @@
+const graphql = require("graphql");
+const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLID } = graphql;
+const mongoose = require("mongoose");
+const Category = mongoose.model("category");
+const Product = mongoose.model("product");
+const CategoryType = require("./types/category_type");
+const ProductType = require("./types/product_type");
+
+const mutations = new GraphQLObjectType({
+  name: "Mutation",
+  fields: {
+    newCategory: {
+        type: CategoryType,
+        args: {
+            id: { type: GraphQLID },
+            name: { type: GraphQLString }
+        },
+        resolve(parentValue, { id, name }) {
+            return new Category({ id, name }).save();
+        }
+    },
+
+    deleteCategory: {
+        type: CategoryType,
+        args: {
+            id: { type: GraphQLID }
+        },
+        resolve(parentValue, { id }) {
+            return Category.findOneAndDelete({ _id: id })
+        }
+    },
+    
+    newProduct: {
+        type: ProductType,
+        args: {
+            id: { type: GraphQLID },
+            name: { type: GraphQLString },
+            description: { type: GraphQLString },
+            weight: { type: GraphQLInt }
+        },
+        resolve(parentValue, { id, name, description, weight }) {
+            return new Product({ id, name, description, weight }).save();
+        }
+    },
+
+    deleteProduct: {
+        type: ProductType,
+        args: {
+            id: { type: GraphQLID }
+        },
+        resolve(parentValue, { id }) {
+            return Product.findOneAndDelete({ _id: id })
+        }
+    }
+  }
+});
+
+module.exports = mutations;
