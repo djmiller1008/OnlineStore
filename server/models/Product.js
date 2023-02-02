5b220b9196d171348a559019c8582e4598033151
David <djmiller1008@yahoo.com>
2023-02-02 14:12:16 +0800

    Add updateProductCategory method

--- server/models/Product.js
+++ server/models/Product.js
@@ -24,4 +24,26 @@ const ProductSchema = new Schema({
     }
 });
 
+ProductSchema.statics.updateProductCategory = (productId, categoryId) => {
+    const Product = mongoose.model("product");
+    const Category = mongoose.model("category");
+
+    return Product.findById(productId).then(product => {
+        if (product.category) {
+            Category.findById(product.category).then(oldCategory => {
+                oldCategory.products.pull(product);
+                return oldCategory.save();
+            })
+        }
+
+        return Category.findById(categoryId).then(newCategory => {
+            product.category = newCategory;
+            newCategory.products.push(product);
+
+            return Promise.all([product.save(), newCategory.save()]).then(([product, newCategory]) => product)
+            
+        });
+    });
+}
+
 module.exports = mongoose.model("product", ProductSchema);
