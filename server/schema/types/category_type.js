201054c7507b1726fb1236448bca1fc85d83507d
David <djmiller1008@yahoo.com>
2023-02-02 14:12:55 +0800

    Add products field to category type

--- server/schema/types/category_type.js
+++ server/schema/types/category_type.js
@@ -1,13 +1,21 @@
 const graphql = require("graphql");
 const mongoose = require("mongoose");
-const { GraphQLObjectType, GraphQLString, GraphQLID } = graphql;
+const Category = mongoose.model("category");
+const ProductType = require("./product_type");
+const { GraphQLObjectType, GraphQLString, GraphQLID, GraphQLList } = graphql;
 
 
 const CategoryType = new GraphQLObjectType({
     name: "CategoryType", 
     fields: () => ({
         _id: { type: GraphQLID },
-        name: { type: GraphQLString }
+        name: { type: GraphQLString },
+        products: {
+            type: new GraphQLList(ProductType),
+            resolve(parentValue, args) {
+                return Category.getProducts(parentValue._id);
+            }
+        }
     })
 });
 
